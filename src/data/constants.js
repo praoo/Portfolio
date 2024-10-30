cc6554692e087d7709826a09da9af9bf39a0dd78
Prabhat Kumar Singh <140948860+praoo@users.noreply.github.com>
2024-10-30 14:43:17 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,12 +1,12 @@
 export const Bio = {
-    name: "Prabhat Singh",
+    name: "Prabhat Kumar Singh",
     roles: [
-      "FullStack Developer",
+      "Frontend Developer",
       "Programmer",
     ],
     description: "To kickstart my career as a software developer, utilizing my strong programming skills and eager to learn from experienced professionals. My goal is to contribute effectively to projects, innovate, and grow as a versatile developer while adding value to the organization.",
     github: "https://github.com/praoo",
-    resume: "https://drive.google.com/file/d/16H2slLCPkFbKppkT7PRNdcnm0d8NkgIB/view?usp=drive_link",
+    resume: "https://drive.google.com/file/d/1w56jx-Cm0TVz4KOKFShQoKyrR_K4hT-L/view",
     linkedin: "https://www.linkedin.com/in/prabhat-kumar-singh-0bb7a8252/",
    
 };
@@ -15,12 +15,18 @@ export const skills = [
     {
       title: "Programming Languages",
       skills: [
-       
+        {
+          name: "C",
+          image: "C-logo.png",
+        },
         {
           name: "C++",
           image: "CPP-logo.png",
         },
-        
+        {
+          name: "Java",
+          image: "java-logo.svg",
+        },
         {
           name: "Python",
           image: "Python-logo.png",
@@ -34,187 +40,3 @@ export const skills = [
           name: "HTML",
           image: "html5-logo.png",
         },
-        {
-          name: "CSS",
-          image:
-            "CSS3-logo.png",
-        },
-        // {
-        //   name: "Bootstrap",
-        //   image:
-        //     "bootstrap-logo.png",
-        // },
-        {
-          name: "JavaScript",
-          image:
-            "JavaScript-logo.png",
-        },
-        {
-          name: "Reactjs",
-          image:
-            "react-logo.png",
-        },
-        
-      ],
-    },
-    {
-      title: "Backend",
-      skills: [
-        {
-          name: "Node Js",
-          image: "nodejs-logo.svg",
-        },
-        {
-          name: "Express Js",
-          image:
-            "expressjs.webp",
-        },
-        {
-          name: "MongoDB",
-          image:
-            "mongo.png",
-        },
-      ],
-    },
-    {
-      title: "Others",
-      skills: [
-        
-        {
-          name: "GitHub",
-          image:
-            "GitHub-logo.png",
-        },
-        {
-          name: "Git",
-          image:
-            "git-logo.png",
-        },
-        {
-          name: "VS Code",
-          image:
-            "vscode-logo.png",
-        },
-      ],
-    },
-];
-  
-export const experiences = [
-    {
-      id: 0,
-      company: "ClimStrip",
-      date: "June 2024 - July 2024",
-      desc: "Working on my virtual internship at ClimpStrip, I contributed as a Web Developer, uses skills React, HTML, CSS, and JavaScript to create user-friendly web interfaces and integrate dynamic features for optimal user experience.",
-      skills: [
-        "React",
-        "HTML",
-        "CSS",
-        "JavaScript",
-        
-        
-      ],
-      doc: "https://drive.google.com/file/d/1qs6Fe9s3Y5DC1BBi4YTzsnXzGy-wP6AG/view",
-    }
-    
-];
-  
-export const education = [
-    {
-      id: 0,
-      img: "hbtu-logo.png",
-      school: "Harcourt Butler Technical University, Kanpur",
-      date: "2022 - 2025",
-      grade: "8.92 CGPA",
-      desc: "I am currently pursuing a Bachelor's degree in Information Technology at Harcourt Butler Technical University, Kanpur with a CGPA of 8.86.  I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
-      degree: "Bachelor of Technology - BTech, Information Technology",
-    },
-    {
-      id: 1,
-      img: "diploma-logo.jpeg",
-      school: "Government Polytechnic Ayodhya",
-      date: "2019 - 2022",
-      grade: "82.38 %",
-      desc: "I completed my Diploma in Mechanical Engineering at Government Polytechnic Ayodhya where I studied some courses of Mechanical engineering.",
-      degree: "Diploma - Mechanical Engineering",
-    },
-    {
-      id: 2,
-      img: "upboard-logo.svg",
-      school: "Veer Bahadur Singh Inter College, Sohas",
-      date: "2019",
-      grade: "90.50 %",
-      desc: "I completed my class 10 education at Veer Bahadur Singh Inter College, Sohas (Maharajganj).",
-      degree: "Highschool - Board of High School & Intermediate Education, UP",
-    },
-];
-  
-export const projects = [
-    {
-      id: 9,
-      title: "AdminElite Dashboard",
-      description:
-        "Created a User-friendly Admin dashboard using MERN stack, featuring full CRUD operations and user authentication capabilities, Designed analytics pages for Products, Customers, Transactions, and Performance, using data visualization tools and also give overview of daily and monthly sales.",
-      image:
-        "admin.png",
-      tags: [
-        "React",
-        "NodeJs",
-        "MongoDB",
-        "ExpressJS",
-      ],
-      category: "web app",
-      github: "https://github.com/praoo/admin_full_stack",
-      webapp: "https://admin-fullstack-e9ny.onrender.com/dashboard",
-    },
-
-
-    {
-      id: 9,
-      title: "IT-JOB Portal",
-      description:
-        `"A Job portal website enabling HR workers to post and manage jobs with user-specific and provied register, login features for users and admins...`,
-      image:
-        "it-job.png",
-        tags: [
-          "React",
-          "NodeJs",
-          "MongoDB",
-          "ExpressJS",
-        ],
-      category: "web app",
-      github: "https://github.com/praoo/IT-JOB_APP",
-      webapp: "https://it-job-portal-7r88.onrender.com/",
-    },
-
-
-    {
-      id: 9,
-      title: "Portfolio",
-      description:
-        "Explore my sleek portfolio, built with HTML5, CSS3, JavaScript, and React.js. Discover my skills and projects through seamless design and interactivity, optimized for any device.",
-      image:
-        "portfolio-logo.png",
-      tags: [
-        "HTML5",
-        "CSS3",
-        "JavaScript",
-        "React",
-      ],
-      category: "web app",
-      github: "https://github.com/praoo/Portfolio",
-      webapp: "#",
-    },
-
-    
-
-   
-];
-  
-// export const TimeLineData = [
-//     { year: 2017, text: "Started my journey" },
-//     { year: 2018, text: "Worked as a freelance developer" },
-//     { year: 2019, text: "Founded JavaScript Mastery" },
-//     { year: 2020, text: "Shared my projects with the world" },
-//     { year: 2021, text: "Started my own platform" },
-// ];
-
